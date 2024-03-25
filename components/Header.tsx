a079b6934095095b874af75a10a260f0a47c7b98
Osaretin Frank <osaretin.frank10@gmail.com>
2024-03-25 11:05:19 +0100

    add mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,30 @@
+"use client"
+import { useNav } from "@/hooks/useNav"
 import { Input, Badge } from "antd"
 import React from "react"
 import { BiBell, BiSearch } from "react-icons/bi"
+import { RxHamburgerMenu } from "react-icons/rx"
 
 export const Header = () => {
+  const { toggleNav } = useNav()
   return (
-    <header className="w-full border-b-[0.5px] border-b-[#D2DBEC] bg-[#fafafa] px-[4rem] py-[2rem]">
+    <header className="w-full border-b-[0.5px] border-b-[#D2DBEC] bg-[#fafafa] md:px-[4rem] px-[1rem] py-[2rem]">
       <div className="flex items-center justify-between">
         <Input
-          className="w-[350px]"
+          className="md:w-[350px] w-[250px]"
           size="large"
           prefix={<BiSearch />}
           placeholder="Search"
         />
-        <Badge count={5}>
-          <BiBell className="text-2xl" />
-        </Badge>
+        <div className="flex items-center gap-6 text-2xl">
+          <Badge count={5}>
+            <BiBell className="text-2xl" />
+          </Badge>
+          <RxHamburgerMenu
+            className="lg:hidden block cursor-pointer"
+            onClick={toggleNav}
+          />
+        </div>
       </div>
     </header>
   )
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,14 +2,29 @@
 import React from "react"
 import { Logo } from "./Logo"
 import { Menu } from "./Menu"
+import { useNav } from "@/hooks/useNav"
 
 export const NavBar = () => {
+  const { showNav } = useNav()
+  const activeClass = showNav ? "translate-x-0" : "-translate-x-full"
   return (
-    <div className="h-screen fixed top-0 left-0 lg:w-[18%] w-[300px] border-r-[0.5px] border-r-[#D2DBEC] z-[10] bg-[#fafafa] py-[2rem] md:px-[2rem] px-[1rem]">
-      <div className="flex items-center justify-center">
-        <Logo />
+    <>
+      {/* Desktop */}
+      <div className="h-screen fixed top-0 left-0 lg:w-[18%] w-[300px] border-r-[0.5px] border-r-[#D2DBEC] z-[10] bg-[#fafafa] py-[2rem] md:px-[2rem] px-[1rem] lg:block hidden">
+        <div className="flex items-center justify-center">
+          <Logo />
+        </div>
+        <Menu />
       </div>
-      <Menu />
-    </div>
+      {/* Mobile */}
+      <div
+        className={`h-screen fixed top-0 left-0 lg:w-[18%] w-[300px] border-r-[0.5px] border-r-[#D2DBEC] z-[10] bg-[#fafafa] py-[2rem] md:px-[2rem] px-[1rem] lg:hidden block ${activeClass} transition-transform duration-300`}
+      >
+        <div className="flex items-center justify-center">
+          <Logo />
+        </div>
+        <Menu />
+      </div>
+    </>
   )
 }
