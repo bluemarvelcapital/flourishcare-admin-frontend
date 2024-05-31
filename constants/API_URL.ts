76df520e1d5b41ff69b1dcc03e9947099e67ec01
RealRichi3 <molunorichie@gmail.com>
2024-05-31 22:40:56 +0100

    update blog route

--- constants/API_URL.ts
+++ constants/API_URL.ts
@@ -1,5 +1,4 @@
-const api_url_dev = "https://flourishcare-api.onrender.com/api/v1"
-const api_url_prod = ""
-
-export const api_url =
-  process.env.NODE_ENV === "development" ? api_url_dev : api_url_prod
+const api_url_dev = "http://localhost:5000/api/v1";
+const api_url_prod = process.env.NEXT_API_URL;
+export const api_url = api_url_prod;
+// process.env.NODE_ENV === "development" ? api_url_dev : api_url_prod;
--- services/blog.service.ts
+++ services/blog.service.ts
@@ -1,60 +1,74 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { api_url } from "@/constants/API_URL"
-import { BlogI } from "@/types/blog"
-import { fetchBaseQueryWithAuth } from "./customQuery"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { api_url } from "@/constants/API_URL";
+import { BlogI } from "@/types/blog";
+import { fetchBaseQueryWithAuth } from "./customQuery";
 
 interface GetBlogPostsResponse {
-    status: 'success',
-    data: {
-        blogPosts: (BlogI & { status: 'draft' | 'published' | 'hidden' })[]
-    }
+  status: "success";
+  data: {
+    blogPosts: (BlogI & { status: "draft" | "published" | "hidden" })[];
+  };
 }
 
 interface UpdateBlogPostResponse {
-    status: 'success',
-    data: {
-        blogPost: BlogI & { status: 'draft' | 'published' | 'hidden' }
-    }
+  status: "success";
+  data: {
+    blogPost: BlogI & { status: "draft" | "published" | "hidden" };
+  };
 }
 
 export const blogApi = createApi({
-    reducerPath: "blogApi",
-    baseQuery: fetchBaseQueryWithAuth({ baseUrl: `${api_url}/blog` }),
-    endpoints: (builder) => {
-        return {
-            getBlogPosts: builder.query<GetBlogPostsResponse, any>({
-                query: () => {
-                    return {
-                        url: "/all",
-                    }
-                },
-            }),
-            updateBlogPost: builder.mutation<UpdateBlogPostResponse, Partial<BlogI> & { blogPostId: string }>({
-                query: (body) => {
-                    console.log({ body });
-                    (body as unknown as Record<string, string>)['previewImage'] = body.preview_image as string;
-                    (body as unknown as Record<string, string>)['coverImage'] = body.cover_image as string;
-                    return {
-                        url: "/",
-                        method: "PATCH",
-                        body: body,
-                    }
-                },
-            }),
-            createBlogPost: builder.mutation<UpdateBlogPostResponse, Omit<BlogI, 'id'>>({
-                query: (body) => {
-                    (body as unknown as Record<string, string>)['previewImage'] = body.preview_image as string;
-                    (body as unknown as Record<string, string>)['coverImage'] = body.cover_image as string;
+  reducerPath: "blogApi",
+  baseQuery: fetchBaseQueryWithAuth({ baseUrl: `${api_url}/blog` }),
+  endpoints: (builder) => {
+    return {
+      getBlogPosts: builder.query<GetBlogPostsResponse, any>({
+        query: () => {
+          return {
+            url: "/",
+          };
+        },
+      }),
+      updateBlogPost: builder.mutation<
+        UpdateBlogPostResponse,
+        Partial<BlogI> & { blogPostId: string }
+      >({
+        query: (body) => {
+          console.log({ body });
+          (body as unknown as Record<string, string>)["previewImage"] =
+            body.preview_image as string;
+          (body as unknown as Record<string, string>)["coverImage"] =
+            body.cover_image as string;
+          return {
+            url: "/",
+            method: "PATCH",
+            body: body,
+          };
+        },
+      }),
+      createBlogPost: builder.mutation<
+        UpdateBlogPostResponse,
+        Omit<BlogI, "id">
+      >({
+        query: (body) => {
+          (body as unknown as Record<string, string>)["previewImage"] =
+            body.preview_image as string;
+          (body as unknown as Record<string, string>)["coverImage"] =
+            body.cover_image as string;
 
-                    return {
-                        url: "/new",
-                        method: "POST",
-                        body: body,
-                    }
-                },
-            }),
-        }
-    },
-})
+          return {
+            url: "/new",
+            method: "POST",
+            body: body,
+          };
+        },
+      }),
+    };
+  },
+});
 
-export const { useGetBlogPostsQuery, useCreateBlogPostMutation, useUpdateBlogPostMutation } = blogApi
+export const {
+  useGetBlogPostsQuery,
+  useCreateBlogPostMutation,
+  useUpdateBlogPostMutation,
+} = blogApi;
