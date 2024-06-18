69f1012b68ef69ab1fc19ec7ae32d8d83b39f174
Moluno-xiii <molunoprogress@gmail.com>
2024-06-19 00:24:28 +0100

    finished children links

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}>
-        <NavBar />
-        <Content>
-          <Header />
-          <div className="md:px-[2.8rem] md:py-[3rem] px-[1rem] py-[2rem] mx-auto container">
-            {children}
-          </div>
-        </Content>
-      </body> */}
-
       <body className="">
           <div className="">
             {children}
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,33 +1,52 @@
-"use client"
-import Link from "next/link"
-import React from "react"
-import { MdOutlineDashboard } from "react-icons/md"
-import { AiOutlineSolution } from "react-icons/ai"
-import { BsJournalMedical } from "react-icons/bs"
-import { LuBookMinus } from "react-icons/lu"
-import { IoBriefcaseOutline } from "react-icons/io5"
-import { RiBookReadLine } from "react-icons/ri"
-import { CiSettings } from "react-icons/ci"
-import { usePathname } from "next/navigation"
-import { Avatar } from "antd"
-import { CgLogOut } from "react-icons/cg"
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { AiOutlineSolution } from "react-icons/ai";
+import { BsJournalMedical } from "react-icons/bs";
+import { LuBookMinus } from "react-icons/lu";
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { RiBookReadLine } from "react-icons/ri";
+import { CiSettings } from "react-icons/ci";
+import { usePathname } from "next/navigation";
+import { Avatar } from "antd";
+import { CgLogOut } from "react-icons/cg";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { IoIosArrowForward } from "react-icons/io";
 
 interface NavItemI {
-  name: string
-  href: string
-  icon: React.ReactNode
+  name: string;
+  href: string;
+  icon: React.ReactNode;
+  childrenLinks?: {
+    name: string;
+    href: string;
+    icon?: React.ReactNode;
+  }[];
 }
 
 const navItems: NavItemI[] = [
   {
     name: "Overview",
-    href: "/",
+    href: "/overview",
     icon: <MdOutlineDashboard />,
   },
   {
     name: "Services",
     href: "/services",
     icon: <AiOutlineSolution />,
+    childrenLinks: [
+      {
+        name: "Listed Services",
+        href: "/services",
+        icon: <AiOutlineSolution />,
+      },
+      {
+        name: "Other Service",
+        href: "/services",
+        icon: <AiOutlineSolution />,
+      },
+    ],
   },
   {
     name: "Appointments",
@@ -54,30 +73,69 @@ const navItems: NavItemI[] = [
     href: "/settings",
     icon: <CiSettings />,
   },
-]
+];
 
 export const Menu = () => {
-  const pathName = usePathname()
-  const active = true
+  const pathName = usePathname();
+  const [expanded, setExpanded] = useState<{ [key: string]: boolean }>({});
+
+  const toggleExpand = (name: string) => {
+    setExpanded((prev) => ({ ...prev, [name]: !prev[name] }));
+  };
+
   return (
     <div className="mt-10">
       <p className="text-primary">Menu</p>
-      <ul className="flex flex-col gap-4 mt-10 ml-4">
-        {navItems.map((item, index) => {
-          const active = pathName === item.href
-          const activeClass = "bg-secondary text-[white] rounded-md "
+      <ul className="flex flex-col gap-4 mt-10">
+        {navItems.map(({ name, href, icon, childrenLinks }, index) => {
+          const active = pathName === href;
+          const activeClass = "bg-secondary text-white rounded-md ";
+          const isExpanded = expanded[name];
           return (
-            <Link
-              href={item.href}
-              key={index}
-              className={active ? activeClass : ""}
-            >
-              <li key={index} className="flex items-center px-4 gap-3 py-3">
-                <div className="text-[1.8rem]">{item.icon}</div>
-                <span>{item.name}</span>
-              </li>
-            </Link>
-          )
+            <React.Fragment key={index}>
+              <div
+                className={`flex items-center justify-between ${
+                  active ? activeClass : ""
+                }`}
+                onClick={() => childrenLinks && toggleExpand(name)}
+              >
+                <Link href={href}>
+                  <li className="flex h-10 items-center gap-3 rounded-md px-4 transition-all duration-300">
+                    <div className="text-[1.8rem]">{icon}</div>
+                    <span>{name}</span>
+                  </li>
+                </Link>
+                {childrenLinks && (
+                  <div className="pr-4">
+                    {isExpanded ? <FiChevronUp /> : <FiChevronDown />}
+                  </div>
+                )}
+              </div>
+              {isExpanded && childrenLinks && (
+                <ul className="ml-5 mt-2  flex flex-col border-l border-light px-2">
+                  {childrenLinks.map((child, childIndex) => {
+                    const childActive = pathName === child.href;
+                    const childActiveClass =
+                      "bg-secondary text-white rounded-md ";
+                    return (
+                      <Link
+                        href={child.href}
+                        key={childIndex}
+                        className={childActive ? childActiveClass : ""}
+                      >
+                        <li className="flex h-10 items-center  justify-between gap-3 rounded-md px-4 transition-all duration-300 ">
+                          {child.name}
+                          <span>
+                            <IoIosArrowForward />
+                          </span>
+                        </li>
+                      </Link>
+                    );
+                  })}
+                </ul>
+              )}
+            </React.Fragment>
+          );
         })}
       </ul>
 
@@ -98,5 +156,5 @@ export const Menu = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
