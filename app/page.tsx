d025a1ac0919881bb45ea73045ea1b4fd1478235
RealRichi3 <molunorichie@gmail.com>
2024-06-21 02:26:20 +0100

    fix: responsiveness for  auth page

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,20 @@ export default function Home() {
   return (
     <main className=" w-full h-full flex justify-between">
       <div className="grid sm:grid-cols-12 bg-white w-full xl:gap-x-14">
-        <div className="md:col-span-7 sm:col-span-6  w-full h-full">
+        <div className="hidden md:block md:col-span-7 sm:col-span-6  w-full h-full">
           <Image
             src={landingImage}
             alt="landing page image"
             className="normal-case w-full h-[100dvh]"
           />
         </div>{" "}
-        <div
-          className="md:col-span-5 sm:col-span-6 w-full text-sm xl:text-lg xl:px-10 px-2 md:px-4 h-[100dvh]"
-        >
+        <div className="px-10 py-10 md:col-span-5 sm:col-span-6 w-full text-sm xl:text-lg xl:px-10 px-2 md:px-4 h-full my-auto flex flex-col justify-center mx-auto max-w-[900px]">
           <header className="flex items-center flex-col">
-            <Image src={Logo} alt="Flourish care logo" className="w-60 " />{" "}
+            <Image
+              src={Logo}
+              alt="Flourish care logo"
+              className="w-[200px] md:w-60  mb-7"
+            />{" "}
             <p className="font font-semibold text-xl xl:text-2xl 2xl:text-4xl mb-1 xl:mb-5">
               Welcome back 👋{" "}
             </p>
@@ -58,20 +60,7 @@ export default function Home() {
             <button className="bg-secondary w-full 2xl:py-3 py-1 rounded-md text-white">
               Login
             </button>
-            <p className="mt-4 2xl:mt-6 text-[#98999A]">
-              Don&apos;t have an account?{" "}
-              <span className="text-secondary">Sign Up</span>
-            </p>
           </div>{" "}
-          <div className="flex flex-row mt-5">
-            <div className="border-b border-[#EEEEEE] flex-1"> </div>
-            <p className="mx-3 text-[#555656]">or</p>
-            <div className="border-b border-[#EEEEEE] flex-1"> </div>{" "}
-          </div>{" "}
-          <button className="flex justify-center w-full hover:bg-secondary mt-5 rounded-md border border-secondary py-1 2xl:py-4 duration-200  hover:border-secondary">
-            <Image src={googleLogo} alt="google logo" />
-            <p className="ml-2">Continue With Google</p>{" "}
-          </button>
         </div>
       </div>
     </main>
